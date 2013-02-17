cf260dfb6d9fea7f69a1d33bb2f2befc0ae6fb70
Samuel Loury <konubinix@gmail.com>
2013-02-17 19:08:28 +0100

    First commit

---
+++ trac_cmd.py
@@ -0,0 +1,243 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import xmlrpclib
+import netrc                    # to access to credentials
+import ConfigParser
+import cmd
+import subprocess
+import tempfile
+import os
+import pprint
+import string
+import json
+import re
+from trhaelppyercthon import TPH
+
+class TracCmd(cmd.Cmd):
+    def __init__(self, server, login="", url=""):
+        cmd.Cmd.__init__(self)
+        self.tph = TPH(server)
+        self.me = login
+        self.pp = pprint.PrettyPrinter(indent=4)
+        self.url = url
+
+    def do_method_list(self, line):
+        for method in self.tph.server.system.listMethods():
+            print method
+
+    def do_method_help(self, method):
+        print self.tph.server.system.methodHelp(method)
+
+    def ticket_attributes_parse_line(self, line):
+        '''Ex: 72 {"summary":"New ticket"}'''
+        content_match = re.match(" *([0-9]+)( +(.+))?", line)
+        ticket_number = content_match.group(1)
+        attributes = content_match.group(3)
+        if attributes:
+            attributes = json.loads(attributes)
+        else:
+            attributes = {}
+
+        return (ticket_number, attributes, )
+
+    def do_ticket_create(self, line):
+        if line:
+            attributes = json.loads(line)
+        else:
+            attributes = {}
+
+        ticket_number = self.tph.ticket_create(attributes, True)
+        if ticket_number is not None:
+            print "Ticket %s created" % (ticket_number)
+        else:
+            print "Creation aborted"
+
+    def do_ticket_clone(self, line):
+        (ticket_number, attributes, ) = self.ticket_attributes_parse_line(line)
+        new_ticket_number = self.tph.ticket_clone(ticket_number, attributes, True)
+        if new_ticket_number:
+            print "Ticket %s, clone of %s, created" % (new_ticket_number,
+                                                       ticket_number)
+        else:
+            print "Clone ticket not created"
+
+    def do_ticket_son_create(self, line):
+        (ticket_number, attributes,) = self.ticket_attributes_parse_line(line)
+        new_ticket_number = self.tph.ticket_son_create(ticket_number, attributes, True)
+        if new_ticket_number:
+            print "Ticket %s, son of %s, created" % (new_ticket_number,
+                                                     ticket_number)
+        else:
+            print "Creation aborted"
+
+    def do_query_ticket(self, line):
+        """owner=owner&status=accepted"""
+        print self.tph.server.ticket.query(line)
+
+    def do_ticket_sons(self, ticket_number):
+        print self.tph.server.ticket.query("parents=~%s" % (ticket_number))
+
+    def do_template_edit(self, line):
+        if self.tph.template_edit():
+            print "Edited template"
+        else:
+            print "Edition aborted"
+
+    def do_template_save(self, filename):
+        if self.tph.template_save(filename):
+            print "Template file saved"
+        else:
+            print "Template file not saved"
+
+    def do_template_open(self, filename):
+        if self.tph.template_open(filename):
+            print "Template file loaded"
+        else:
+            print "Template file not loaded"
+
+    def do_iam(self, line):
+        self.me = line
+
+    def do_whoami(self, line):
+        print self.me
+
+    def do_ticket_mine(self, line):
+        self.do_query_ticket("owner=%s&status=accepted" % self.me)
+
+    def do_ticket_mine_pending(self, line):
+        self.do_query_ticket("owner=%s&status=assigned" % self.me)
+
+    def do_ticket_close(self, ticket):
+        if self.tph.ticket_close(int(ticket)):
+            print "Closed ticket %s" % (ticket)
+        else:
+            print "Failed to close ticket"
+
+    def do_list_attachment(self, ticket):
+        print self.tph.server.ticket.listAttachments(int(ticket))
+
+    def do_list_components(self, filter):
+        self.pp.pprint(
+            self.tph.list_components(filter)
+        )
+
+    def do_list_resolution(self, line):
+        self.pp.pprint(self.tph.server.ticket.resolution.getAll())
+
+    def do_list_priority(self, line):
+        self.pp.pprint(self.tph.server.ticket.priority.getAll())
+
+    def do_list_status(self, line):
+        self.pp.pprint(self.tph.server.ticket.status.getAll())
+
+    def do_list_type(self, line):
+        self.pp.pprint(self.tph.server.ticket.type.getAll())
+
+    def do_get_actions(self, ticket):
+        print self.tph.server.ticket.getActions(int(ticket))
+
+    def do_verbatim(self, line):
+        exec("print self.tph.server.%s" % line)
+
+    def do_ticket_edit(self, ticket_number):
+        if self.tph.ticket_edit(int(ticket_number)):
+            print "Ticket %s edited" % (ticket_number)
+        else:
+            print "Edition aborted"
+
+    def do_ticket_changelog(self, line):
+        """Args: ticket number_of_changes
+        number_of_changes default to 10
+        number_of_changes set to 0 means no limit
+        """
+        match = re.match(" *([0-9]+)( +([0-9]+))?", line)
+        ticket_number = match.group(1)
+        lines = match.group(3) or "10"
+        changelog = self.tph.server.ticket.changeLog(ticket_number)
+        changelog.reverse()
+        if lines != "0":
+            changelog = changelog[:int(lines)]
+        self.pp.pprint(changelog)
+
+    def do_web(self, ticket):
+        url="%(URL)s/ticket/%(TICKET)s" % {
+            "URL" : self.url,
+            "TICKET" : ticket,
+        }
+        subprocess.Popen(
+            [os.environ['BROWSER'],
+             url
+         ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+
+    def do_change_parents(self, ticket):
+        self.change_part(ticket, 'parents')
+
+    def do__dump_ticket(self, ticket):
+        self.pp.pprint(self.tph.ticket_get(int(ticket)))
+
+    def do_ticket_search(self, query):
+        self.pp.pprint(
+            self.tph.server.search.performSearch(
+                query,
+                ["ticket",]
+            )
+        )
+
+    def do_wiki_search(self, query):
+        self.pp.pprint(
+            self.tph.server.search.performSearch(
+                query,
+                ["wiki",]
+            )
+        )
+
+    def do__api(self, ticket):
+        subprocess.Popen(
+            [os.environ['BROWSER'],
+             "%(URL)s/login/xmlrpc" % {"URL" : self.url}],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+
+
+    def do_EOF(self, line):
+        return True
+
+def main():
+    config = ConfigParser.ConfigParser()
+    config.optionxform = str    # keys not converted into lower case
+    config.read(os.environ.get("TRAC_CMDRC",
+                               os.path.expanduser("~/.trac_cmdrc.conf")))
+    url = config.get("server", "url")
+    protocol = config.get("server", "protocol")
+    trac_path = config.get("server", "trac_path")
+    net = netrc.netrc()
+    (login, account, password) = \
+                                 net.authenticators(
+                                     "%s://%s" % (protocol, url,)
+                                 )
+    server = \
+             xmlrpclib.ServerProxy(
+                 "%(PROTOCOL)s://%(LOGIN)s:%(PASS)s@%(URL)s%(PATH)s/login/xmlrpc"
+                 % {"LOGIN" : login,
+                    "PASS" : password,
+                    "PROTOCOL" : protocol,
+                    "PATH" : trac_path,
+                    "URL" : url,
+                }
+             )
+    TracCmd(server,
+            login=login,
+            url="%(PROTOCOL)s://%(URL)s%(PATH)s" % {
+                "PROTOCOL" : protocol,
+                "URL" : url,
+                "PATH" : trac_path,
+            }
+        ).cmdloop()
+
+if __name__ == '__main__':
+    main()
---
+++ trhaelppyercthon.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import netrc
+import tempfile
+import subprocess
+import os
+import re
+
+class TPH(object):
+    def __init__(self, server):
+        self.server = server
+        self.fields = (
+            'summary',
+            'component',
+            'estimatedhours',
+            'keywords',
+            'cc',
+            'milestone',
+            'owner',
+            'parents',
+            'blockedby',
+            'blocking',
+            'priority',
+            'reporter',
+            'resolution',
+            'startdate',
+            'status',
+            'type',
+        )
+        self.template_attributes = {}
+
+    def edit_comment(self, comment="", info=""):
+        info_string = "\n".join([
+            "# " + line for line in info.splitlines()
+        ])
+        info_string = "\n# lines beginning with # are avoided\n" + info_string
+        comment = self.edit(comment + info_string)
+        # remove the lines beginning with #
+        new_comment_lines = [
+            line for line in comment.splitlines()
+            if not re.search("^#", line)
+        ]
+        return "\n".join(new_comment_lines)
+
+    def edit(self, content, suffix=".wiki", prefix=""):
+        temp_file = tempfile.NamedTemporaryFile(prefix=prefix, suffix=suffix, delete=False)
+        temp_file.write(content)
+        temp_file.close()
+        rc = subprocess.call([os.environ["EDITOR"], temp_file.name])
+        if rc == 0:
+            # get the new content and return it
+            with open(temp_file.name, "r") as tfile:
+                new_content = tfile.read()
+
+        else:
+            new_content = None
+            os.unlink(temp_file.name)
+
+        return new_content
+
+    def attributes_dump(self, attributes, ignore_empty=False):
+        content = ""
+        for field in self.fields:
+            value = attributes.get(field, "")
+            if value or not ignore_empty:
+                content = content + field+"="+value+"\n"
+        content = content + attributes.get("description", "")
+        return content
+
+    def attributes_load(self, string):
+        attributes = {}
+        content = string.splitlines()
+        field_regexp = "^([^=]+)=(.*)$"
+        match = re.match(field_regexp, content[0])
+        while match:
+            field_name = match.group(1)
+            field_value = match.group(2)
+            attributes[field_name] = field_value
+            content.pop(0)
+            if len(content) > 0:
+                match = re.match(field_regexp, content[0])
+            else:
+                match = None
+
+        # the remaining of the content is the description
+        description = "\n".join(content)
+        if description:
+            # avoid emptying the description
+            attributes["description"] = description
+        return attributes
+
+    def attributes_edit(self, attributes, prefix="", ignore_empty=False):
+        attributes_string = self.edit(
+            self.attributes_dump(attributes, ignore_empty=ignore_empty),
+            prefix=prefix+"_"
+        )
+        if attributes_string == "":
+            return None
+
+        return self.attributes_load(attributes_string)
+
+    def ticket_create(self, p_attributes, use_editor=False):
+        attributes = self.template_attributes.copy()
+        attributes.update(p_attributes)
+        summary = attributes.get("summary","Summary")
+        description = attributes.get("description","Description\n")
+        if use_editor:
+            attributes["summary"] = summary
+            attributes["description"] = description
+            attributes = self.attributes_edit(attributes)
+            if attributes == None:
+                return None
+
+        return self.server.ticket.create(
+            summary,
+            description,
+            attributes)
+
+    def ticket_clone(self, ticket_number, attributes, use_editor=False):
+        # get the ticket to clone
+        ticket = self.get(ticket_number)
+        ticket_attributes = ticket[3]
+        # remove what is not relevant in the ticket
+        for key in ("changetime", "_ts", "time",):
+            del ticket_attributes[key]
+            # addition of the overriding attributes
+            ticket_attributes.update(attributes)
+            # I am the new reporter of the ticket
+            ticket_attributes["reporter"] = self.me
+            #self.pp.pprint(ticket_attributes)
+
+        return self.ticket_create(ticket_attributes, use_editor)
+
+    def ticket_son_create(self, ticket_number, attributes, use_editor=False):
+        # get the ticket to clone
+        ticket = self.get(ticket_number)
+        ticket_old_attributes = ticket[3]
+        # get only the relevant info to copy from the parent ticket
+        ticket_attributes = {
+            "cc" : ticket_old_attributes["cc"],
+            "component" : ticket_old_attributes["component"],
+            "estimatedhours" : ticket_old_attributes["estimatedhours"],
+            "keywords" : "",    # new set of keywords
+            "milestone" : ticket_old_attributes["milestone"],
+            "owner" : ticket_old_attributes["owner"],
+            "parents" : "#" + str(ticket[0]),
+            "priority" : ticket_old_attributes["priority"],
+            "reporter" : self.me,
+            "summary" : ticket_old_attributes["summary"] + " - Sub ticket",
+            "status" : "new",
+            "description" : ticket_old_attributes["description"],
+            'type': 'defect',
+        }
+        ticket_attributes.update(attributes)
+        return self.ticket_create(ticket_attributes, use_editor)
+    def ticket_batch_set(self, id_list, attributes):
+        for id in id_list:
+            self.server.ticket.update(int(id),
+                                      "",
+                                      attributes
+                                  )
+
+    def ticket_get(self, ticket):
+        return self.server.ticket.get(ticket)
+
+    def ticket_batch_edit(self, id_list):
+        for id in id_list:
+            ticket = self.ticket_get(id)
+            attributes = self.attributes_edit(ticket[3])
+            self.server.ticket.update(int(id),
+                                      "",
+                                      attributes
+                                  )
+
+    def template_edit(self):
+        attributes = \
+                     self.attributes_edit(self.template_attributes)
+        if attributes:
+            self.template_attributes = attributes
+            return True
+        else:
+            return False
+
+    def template_save(self, file_name):
+        assert not os.path.exists(file_name)
+        with open(file_name, "w") as file:
+            file.write(self.attributes_dump(self.template_attributes))
+
+        return True
+
+    def template_open(self, file_name):
+        assert os.path.exists(file_name)
+        with open(file_name, "r") as file:
+            self.template_attributes = self.attributes_load(file.read())
+
+        return True
+
+    def ticket_close(self, ticket):
+        content = self.tph.edit("fixed\n\nComment")
+        if content == "":
+            return False
+        content_split = content.splitlines()
+        resolution = content_split[0]
+        comment = "\n".join(content_split[2:])
+        self.server.ticket.update(ticket,
+                                  comment,
+                                  {
+                                      'action':'resolve',
+                                      'action_resolve_resolve_resolution': resolution,
+                                  }
+                              )
+        return True
+
+    def ticket_edit(self, ticket_number):
+        ticket = self.ticket_get(ticket_number)
+        attributes = ticket[3]
+        attributes = self.attributes_edit(attributes, str(ticket_number))
+        if attributes:
+            attributes_string = self.attributes_dump(attributes)
+            comment = self.edit_comment(info=attributes_string)
+            self.server.ticket.update(
+                ticket[0],
+                comment,
+                attributes
+            )
+            return True
+        else:
+            return False
+
+    def list_components(self, filter):
+        return [
+            comp for comp in self.server.ticket.component.getAll()
+            if re.search(filter, comp, flags=re.I)
+        ]
